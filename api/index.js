ec4d2018daeebdb35a24dd78f37716b142ab3017
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2023-01-27 19:27:38 +0700

    Refactor for Vercel deployment

--- api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const App = express();
-
-const AppRoute = require("./routes");
-AppRoute(App);
-
-module.exports = App;
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require("express");
+const App = express();
 const PORT = 8080;
-const App = require("./api/index");
+
+const AppRoute = require("./api/routes");
+AppRoute(App);
 
 App.listen(PORT,()=>{console.log(`App listening on port ${PORT}`)});
+
+// Export the Express API
+module.exports = App;
