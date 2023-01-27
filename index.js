fa0df038108ec6297c65346c51c8ed61a5738ef6
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2023-01-27 16:45:08 +0700

    Edit index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const PORT = 8080;
 const App = require("./api/index");
 
-//App.listen(PORT,()=>{console.log(`App listening on port ${PORT}`)});
-module.exports = App;
+App.listen(PORT,()=>{console.log(`App listening on port ${PORT}`)});
