45a53b64898dc7ad0e302c74a9b8ca49c3f162d9
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2023-01-28 00:51:07 +0700

    Change response of / to hello world again

--- api/routes.js
+++ api/routes.js
@@ -2,7 +2,7 @@ const UserInfo = require("./controllers/User/UserInfo")
 
 function AppRoute(app) {
     app.route('/')
-        .get((req, res) => { res.send({ msg: process.env.MDB_USERNAME }) })
+        .get((req, res) => { res.send({ msg: "Hello world!" }) })
 
     app.route('/user/userinfo/:userId')
         .get(async function (req, res) { res.send(await UserInfo(req.params.userId)) })
