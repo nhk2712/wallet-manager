dc0fc70b86a5d7ea6510d092bf33b197972e180b
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2023-01-28 00:49:07 +0700

    Edit to test dotenv

--- api/routes.js
+++ api/routes.js
@@ -2,7 +2,7 @@ const UserInfo = require("./controllers/User/UserInfo")
 
 function AppRoute(app) {
     app.route('/')
-        .get((req, res) => { res.send({ msg: "Hello world!" }) })
+        .get((req, res) => { res.send({ msg: process.env.MDB_USERNAME }) })
 
     app.route('/user/userinfo/:userId')
         .get(async function (req, res) { res.send(await UserInfo(req.params.userId)) })
