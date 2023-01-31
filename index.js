d3a05c2b78575181ec12adf3fd8ecb9cd6fc02ff
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2023-01-31 18:03:18 +0700

    Add CORS

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const express = require("express");
 const App = express();
 const PORT = 8080;
 
+const cors = require("cors");
+const FrontendUrl = process.env.FRONTEND_URL;
+App.use(cors({
+    origin:FrontendUrl.substring(0,FrontendUrl.length-1),
+    methods:'GET'
+}));
+
 const AppRoute = require("./api/routes");
 AppRoute(App);
 
